8121f050a33806fb6928cbb2069067e14faa3c09
Daniele Lenares <hi@dnlnrs.dev>
2022-01-18 17:58:39 +0100

    fix: edit responsive view for button

--- src/components/elements/Link.svelte
+++ src/components/elements/Link.svelte
@@ -8,6 +8,6 @@
   }
 </script>
 
-<a href={externalUrl()} target="_blank" class="flex items-center justify-around py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+<a href={externalUrl()} target="_blank" class="flex items-center justify-center py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
   <slot />
 </a>
