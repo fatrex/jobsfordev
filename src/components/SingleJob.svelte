cdedb798cfed8e059d980ffb77d77e2a2a8fbbe1
Daniele Lenares <hi@dnlnrs.dev>
2022-01-18 17:58:50 +0100

    fix: edit ui view for job card

--- src/components/SingleJob.svelte
+++ src/components/SingleJob.svelte
@@ -6,7 +6,7 @@
   const companyLogo = job.company_logo.files[0].file.url
   const companyName = job.company_name.title[0].plain_text
   const role = job.role.rich_text[0].plain_text
-  const seniority = job.seniority.select.name
+  const seniority = job.seniority.select?.name
 
   const officePresence = job.office_presence.select.name
   // const salaryMin = job.salary_min.number
@@ -18,7 +18,7 @@
   const link = job.link.url
 </script>
 
-<div class="grid grid-flow-row lg:grid-flow-col lg:grid-cols-12 m-2 p-2 rounded-md border-2">
+<div class="grid grid-flow-row lg:grid-flow-col lg:grid-cols-12 m-2 p-2 rounded-md border-2 border-gray-300 shadow-sm hover:shadow-md hover:bg-gray-50">
   <div class="image col-span-1 items-center pb-2 lg:pb-0 lg:justify-center flex">
     <div class="h-14 w-14 rounded-full bg-center bg-cover flex" style="background-image:url({companyLogo})"></div>
     <em class="block lg:hidden pl-2">{companyName}</em>
@@ -26,7 +26,10 @@
   <div class="content col-span-5">
     <em class="hidden lg:block">{companyName}</em>
     <h3 class="text-xl text-gray-900 font-semibold">
-      {role} | {seniority}
+      {role} 
+      {#if seniority}
+      <span class="text-gray-400">|</span> {seniority}
+      {/if}
     </h3>
     <div class="divide-x divide-double flex flex-row">
       <p class="text-base text-gray-700 font-light pr-2">
@@ -42,26 +45,32 @@
     <!-- Main Skills -->
     <div class="skills flex flex-row place-items-center">
       <i class='bx bx-check-double'></i>
-      {#each mainSkills as skill, i}
-      <span class="px-2 ml-2 text-base rounded-full text-white  bg-indigo-500 border-transparent border-2 ">
-        {skill}
-      </span>
-      {/each}
+      <div class="overflow-x-auto">
+        {#each mainSkills as skill, i}
+        <span class="px-2 ml-2 text-base rounded-full text-white  bg-indigo-500 border-transparent border-2 ">
+          {skill}
+        </span>
+        {/each}
+      </div>
     </div>
     <!-- Additional Skills -->
     <div class="skills flex flex-row items-center">
+      {#if additionalSkills.length > 0}
       <i class='bx bx-check'></i>
-      {#each additionalSkills as skill, i}
-      <span class="px-2 ml-2 text-xs rounded-full text-black  bg-indigo-200 border-indigo-300 border-2 ">
-        {skill}
-      </span>
-      {/each}
+      <div class="overflow-x-auto">
+        {#each additionalSkills as skill, i}
+        <span class="px-2 ml-2 text-xs rounded-full text-black  bg-indigo-200 border-indigo-300 border-2 ">
+          {skill}
+        </span>
+        {/each}
+      </div>
+      {/if}
     </div>
   </div>
 
   <div class="actions col-span-5 lg:px-2 lg:col-span-2 items-center lg:flex pt-3 lg:pt-0">
     <Link href={link}>
-      <p>Candidati</p>
+      <p>Vedi</p>
       <i class='ml-2 bx bx-link-external'></i>
     </Link>
   </div>
