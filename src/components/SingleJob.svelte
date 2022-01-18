1a6a6fd1a2e76820e0e42ee59ce0b28ae4da957a
Daniele Lenares <hi@dnlnrs.dev>
2022-01-18 09:42:20 +0100

    feat: create link component

--- src/components/SingleJob.svelte
+++ src/components/SingleJob.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Link from './elements/Link.svelte'
+
   export let job
 
   const companyLogo = job.company_logo.files[0].file.url
@@ -39,14 +41,14 @@
   <div class="grid grid-flow-row gap-2 col-span-5 pt-3 lg:pt-0">
     <!-- Main Skills -->
     <div class="skills flex flex-row place-items-center">
-      <i class="las la-check-double text-xl"></i>
+      <i class='bx bx-check-double'></i>
       {#each mainSkills as skill, i}
       <div class="rounded-full text-sm bg-teal-300 text-black p-1 px-2 border-2 border-teal-400 font-light ml-2">{skill}</div>
       {/each}
     </div>
     <!-- Additional Skills -->
     <div class="skills flex flex-row items-center">
-      <i class="las la-check text-xl"></i>
+      <i class='bx bx-check'></i>
       {#each additionalSkills as skill, i}
       <div class="rounded-full text-xs bg-blue-200 text-black px-2 border-2 border-blue-400 font-light ml-2">{skill}</div>
       {/each}
@@ -54,11 +56,9 @@
   </div>
 
   <div class="actions col-span-5 lg:px-2 lg:col-span-2 items-center lg:flex pt-3 lg:pt-0">
-    <a href="{link}" target="_blank" class="flex rounded text-white bg-blue-600 p-3 justify-center">
-      Candidati
-      <svg xmlns="http://www.w3.org/2000/svg" class="pl-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-      </svg>
-    </a>
+    <Link href={link}>
+      <p>Candidati</p>
+      <i class='ml-2 bx bx-link-external'></i>
+    </Link>
   </div>
 </div>
