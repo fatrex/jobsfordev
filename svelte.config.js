5dde56548fdf5693d259f5720ea717030b0850e4
Daniele Lenares <hi@dnlnrs.dev>
2022-02-10 09:35:11 +0100

    feat(svelte): add path aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from "@sveltejs/adapter-vercel"
+import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#app'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#app",
+    vite: () => ({
+      resolve: {
+        alias: {
+          '@': path.join(process.cwd(), './src')
+        }
+      }
+    }),
+  },
 };
 
 export default config;
