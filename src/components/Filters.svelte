099403e8b0ce7c49d7a1c94fe8719c3ec11d3adc
Daniele Lenares <hi@dnlnrs.dev>
2022-02-18 08:02:29 +0100

    fix: edit filter logic

--- src/components/Filters.svelte
+++ src/components/Filters.svelte
@@ -1,22 +1,54 @@
 <script>
-  import RangeSlider from "svelte-range-slider-pips";
+  import RangeSlider from "svelte-range-slider-pips"
+  import Select from 'svelte-select'
 
-  import { filteredJobs, originalJobs } from '@/stores/job'
+  import { filteredJobs, originalJobs, technologies } from '@/stores/job'
+
+  let filteredSalary = $originalJobs
+  let filteredTech = $originalJobs
 
   function handleSalaryChange (e) {
     const { values } = e.detail
     const minSalary = values[0]
     const maxSalary = values[1]
 
-    const items = $originalJobs.filter(item =>
-      minSalary <= (item.salary.formula.number / 1000) && (item.salary.formula.number / 1000) <= maxSalary
+    if (minSalary === 20 && maxSalary === 100) {
+      filteredSalary = $originalJobs
+    } else {
+      const items = $originalJobs.filter(item =>
+        minSalary <= (item.salary.formula.number / 1000) && (item.salary.formula.number / 1000) <= maxSalary
+      )
+      filteredSalary = items
+    }
+    setItems()
+  }
+
+  function handleTechSelect (e) {
+    if (!e.detail) {
+      filteredTech = $originalJobs
+    } else {
+      const values = e.detail.map(val => val.value)
+      const items = $originalJobs.filter(item => 
+        item.main_skills.multi_select.filter(skill => {
+          return values.includes(skill.name)
+        }).length > 0
+      )
+      filteredTech = items
+    }
+    setItems()
+  }
+
+  function setItems () {
+    console.log(filteredSalary, filteredTech)
+    const items = filteredSalary.filter(item => 
+      filteredTech.filter(job => job.id === item.id).length > 0 
     )
     filteredJobs.set(items)
   }
 </script>
 
 <div class="flex flex-col lg:flex-row lg:justify-between items-center bg-gray-100 rounded-lg p-4 sm:p-8 mt-10">
-  <div class="w-96">
+  <div class="w-full lg:w-1/3">
     <small>RAL desiderata</small>
     <RangeSlider
       float
@@ -30,5 +62,9 @@
       prefix="€"
       suffix="k"
       first='label' last='label' on:change={handleSalaryChange} />
+  </div>
+  <div class="w-full pt-10 lg:p-0 lg:w-1/3">
+    <small>Tecnologie preferite (max. 4)</small>
+    <Select items={$technologies} isMulti placeholder="Seleziona..." on:select={handleTechSelect} />
   </div>
 </div>
--- src/stores/job.js
+++ src/stores/job.js
@@ -2,3 +2,4 @@ import { writable } from "svelte/store"
 
 export const filteredJobs = writable([])
 export const originalJobs = writable([])
+export const technologies = writable([])
