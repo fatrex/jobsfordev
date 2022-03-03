df68c56ecb507ea34f7bc623ec7f4fa7779f80dc
Daniele Lenares <hi@dnlnrs.dev>
2022-03-03 07:13:41 +0100

    fix: revert logiv

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,6 @@
 <script>
-  import { dev } from '$app/env';
-  import { onMount } from 'svelte'
   import '../app.css'
   import Footer from '../components/Footer.svelte'
-
-  onMount(() => {
-    if (dev === false) {
-      window._paq.push(['trackPageView']);
-    }
-  })
 </script>
 
 <div class="container mx-auto mt-2">
