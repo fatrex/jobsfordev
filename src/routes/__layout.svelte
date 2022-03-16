5d80f9b984495b2dfe0703f89848edb55d0578a9
Daniele Lenares <hi@dnlnrs.dev>
2022-03-16 09:30:34 +0100

    feat: add umami

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,12 @@
 <script>
   import '../app.css'
   import Footer from '../components/Footer.svelte'
+  const { VITE_UMAMI_SITE_ID } = import.meta.env
 </script>
+
+<svelte:head>
+  <script async defer data-website-id="{VITE_UMAMI_SITE_ID}" src="https://trk.codebits.it/umami.js"></script>
+</svelte:head>
 
 <div class="container mx-auto mt-2">
   <slot />
