6c650ad27d9b2afdc14635d90a37b1669454660b
Daniele Lenares <hi@dnlnrs.dev>
2022-03-18 18:02:13 +0100

    feat(fe): update deployment

--- packages/frontend/svelte.config.js
+++ packages/frontend/svelte.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({}),
+    adapter: adapter(),
     vite: () => ({
       resolve: {
         alias: {
