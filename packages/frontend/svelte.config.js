a27a907a81f7d92ccda594bb4089b083d5ff02e0
Daniele Lenares <hi@dnlnrs.dev>
2022-03-18 17:38:44 +0100

    feat(fe): edit build frontend

--- packages/frontend/svelte.config.js
+++ packages/frontend/svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from "@sveltejs/adapter-auto"
+import adapter from "@sveltejs/adapter-node"
 import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({}),
     vite: () => ({
       resolve: {
         alias: {
