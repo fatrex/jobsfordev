e0b02671e99de6226dc0c95b1ef4adee9fc20c58
Daniele Lenares <hi@dnlnrs.dev>
2022-03-18 11:44:21 +0100

    feat(api): edit schema

--- packages/api/schema.ts
+++ packages/api/schema.ts
@@ -1,42 +1,11 @@
-/*
-Welcome to the schema! The schema is the heart of Keystone.
-
-Here we define our 'lists', which will then be used both for the GraphQL
-API definition, our database tables, and our Admin UI layout.
-
-Some quick definitions to help out:
-A list: A definition of a collection of fields with a name. For the starter
-  we have `User`, `Post`, and `Tag` lists.
-A field: The individual bits of data on your list, each with its own type.
-  you can see some of the lists in what we use below.
-
-*/
-
-// Like the `config` function we use in keystone.ts, we use functions
-// for putting in our config so we get useful errors. With typescript,
-// we get these even before code runs.
-// We are using Typescript, and we want our types experience to be as strict as it can be.
-// By providing the Keystone generated `Lists` type to our lists object, we refine
-// our types to a stricter subset that is type-aware of other lists in our schema
-// that Typescript cannot easily infer.
 import { Lists } from '.keystone/types';
 import { list, graphql } from '@keystone-6/core';
-// We're using some common fields in the starter. Check out https://keystonejs.com/docs/apis/fields#fields-api
-// for the full list of fields.
 import {
   image, password, relationship, text, timestamp, select, integer, virtual
 } from '@keystone-6/core/fields';
 
-
-
-// We have a users list, a blogs list, and tags for blog posts, so they can be filtered.
-// Each property on the exported object will become the name of a list (a.k.a. the `listKey`),
-// with the value being the definition of the list, including the fields.
 export const lists: Lists = {
-  // Here we define the user list.
   User: list({
-    // Here are the fields that `User` will have. We want an email and password so they can log in
-    // a name so we can refer to them, and a way to connect users to posts.
     fields: {
       name: text({ validation: { isRequired: true } }),
       email: text({
@@ -44,15 +13,15 @@ export const lists: Lists = {
         isIndexed: 'unique',
         isFilterable: true,
       }),
-      // The password field takes care of hiding details and hashing values
       password: password({ validation: { isRequired: true } }),
-      // Relationships allow us to reference other lists. In this case,
-      // we want a user to have many posts, and we are saying that the user
-      // should be referencable by the 'author' field of posts.
-      // Make sure you read the docs to understand how they work: https://keystonejs.com/docs/guides/relationships#understanding-relationships
-      // posts: relationship({ ref: 'Post.author', many: true }),
     },
-    // Here we can configure the Admin UI. We want to show a user's name and posts in the Admin UI
+    access: {
+      operation: {
+        create: ({ session }) => {
+          return !!session
+        }
+      }
+    },
     ui: {
       listView: {
         initialColumns: ['name'],
@@ -60,6 +29,13 @@ export const lists: Lists = {
     },
   }),
   Company: list({
+    access: {
+      operation: {
+        create: ({ session }) => {
+          return !!session
+        }
+      }
+    },
     fields: {
       name: text({ validation: { isRequired: true } }),
       logo: image()
@@ -67,6 +43,14 @@ export const lists: Lists = {
   }),
   JobOffer: list({
     fields: {
+      status: select({
+        defaultValue: 'draft',
+        options: [
+          { label: 'Draft', value: 'draft' },
+          { label: 'Published', value: 'published'},
+          { label: 'Expired', value: 'expired' }
+        ]
+      }),
       company: relationship({ ref: 'Company' }),
       role: text({ validation: { isRequired: true } }),
       seniority: select({
@@ -96,6 +80,7 @@ export const lists: Lists = {
           }
         })
       }),
+      originalUrl: text({ validation: { isRequired: true } }),
       createdAt: timestamp({
         defaultValue: {
           kind: 'now'
@@ -108,76 +93,4 @@ export const lists: Lists = {
       name: text({ validation: { isRequired: true } })
     },
   }),
-  // Our second list is the Posts list. We've got a few more fields here
-  // so we have all the info we need for displaying posts.
-  // Post: list({
-  //   fields: {
-  //     title: text(),
-  //     // Having the status here will make it easy for us to choose whether to display
-  //     // posts on a live site.
-  //     status: select({
-  //       options: [
-  //         { label: 'Published', value: 'published' },
-  //         { label: 'Draft', value: 'draft' },
-  //       ],
-  //       // We want to make sure new posts start off as a draft when they are created
-  //       defaultValue: 'draft',
-  //       // fields also have the ability to configure their appearance in the Admin UI
-  //       ui: {
-  //         displayMode: 'segmented-control',
-  //       },
-  //     }),
-  //     // The document field can be used for making highly editable content. Check out our
-  //     // guide on the document field https://keystonejs.com/docs/guides/document-fields#how-to-use-document-fields
-  //     // for more information
-  //     content: document({
-  //       formatting: true,
-  //       layouts: [
-  //         [1, 1],
-  //         [1, 1, 1],
-  //         [2, 1],
-  //         [1, 2],
-  //         [1, 2, 1],
-  //       ],
-  //       links: true,
-  //       dividers: true,
-  //     }),
-  //     publishDate: timestamp(),
-  //     // Here is the link from post => author.
-  //     // We've configured its UI display quite a lot to make the experience of editing posts better.
-  //     author: relationship({
-  //       ref: 'User.posts',
-  //       ui: {
-  //         displayMode: 'cards',
-  //         cardFields: ['name', 'email'],
-  //         inlineEdit: { fields: ['name', 'email'] },
-  //         linkToItem: true,
-  //         inlineCreate: { fields: ['name', 'email'] },
-  //       },
-  //     }),
-  //     // We also link posts to tags. This is a many <=> many linking.
-  //     tags: relationship({
-  //       ref: 'Tag.posts',
-  //       ui: {
-  //         displayMode: 'cards',
-  //         cardFields: ['name'],
-  //         inlineEdit: { fields: ['name'] },
-  //         linkToItem: true,
-  //         inlineConnect: true,
-  //         inlineCreate: { fields: ['name'] },
-  //       },
-  //       many: true,
-  //     }),
-  //   },
-  // }),
-  // Our final list is the tag list. This field is just a name and a relationship to posts
-  // Tag: list({
-  //   ui: {
-  //     isHidden: true,
-  //   },
-  //   fields: {
-  //     name: text(),
-  //     posts: relationship({ ref: 'Post.tags', many: true }),
-  //   },
-  // }),
 };
