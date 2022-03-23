4569da8139d4ab3ac33beac22586c6c96c409cbf
Daniele Lenares <hi@dnlnrs.dev>
2022-03-23 18:21:33 +0100

    feat(fe): add ackee base logic

--- packages/frontend/src/routes/__layout.svelte
+++ packages/frontend/src/routes/__layout.svelte
@@ -1,12 +1,25 @@
 <script>
   import '../app.css'
   import Footer from '../components/Footer.svelte'
-  const { VITE_UMAMI_SITE_ID } = import.meta.env
+  const { VITE_ACKEE_SERVER, VITE_ACKEE_ID } = import.meta.env
+  
+  import { onMount } from 'svelte'
+
+  onMount(async () => {
+    const ackeeTracker = await import('ackee-tracker')
+    const trackerInstance = ackeeTracker.create(VITE_ACKEE_SERVER, {
+      detailed: false,
+      ignoreLocalhost: true,
+      ignoreOwnVisits: false,
+    })
+
+    trackerInstance.record(VITE_ACKEE_ID, {
+      siteLocation: window.location.href,
+      siteReferrer: document.referrer
+    })
+  })
 </script>
 
-<svelte:head>
-  <script async defer data-website-id="{VITE_UMAMI_SITE_ID}" src="https://trk.codebits.it/umami.js"></script>
-</svelte:head>
 
 <div class="container mx-auto mt-2">
   <slot />
